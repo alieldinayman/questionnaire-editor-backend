8963dadf73321215af8b46742ce31b64add3854f
Alieldin Ayman <alieldinayman@gmail.com>
2022-05-12 21:58:59 +0200

    Change request size limit to be an environment variable

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
 
     // Increase the request size limit to 60mb
-    app.use(json({ limit: '60mb' }));
-    app.use(urlencoded({ extended: true, limit: '60mb' }));
+    app.use(json({ limit: process.env.REQUEST_SIZE_LIMIT }));
+    app.use(urlencoded({ extended: true, limit: process.env.REQUEST_SIZE_LIMIT }));
 
     await app.listen(process.env.PORT || 3001);
 }
