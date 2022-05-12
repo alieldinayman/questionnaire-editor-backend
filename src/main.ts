3fe07ceb4f1957aacf749726ac925dc69073b53b
Alieldin Ayman <alieldinayman@gmail.com>
2022-05-12 21:54:39 +0200

    Increase the default request size limit, and set CORS and env port value

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const app = await NestFactory.create(AppModule, { cors: true });
+
+    // Increase the request size limit to 60mb
+    app.use(json({ limit: '60mb' }));
+    app.use(urlencoded({ extended: true, limit: '60mb' }));
+
+    await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
